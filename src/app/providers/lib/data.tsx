ac2ca43b67be7e4cc18cf5a708bd062887b69d01
Sergey Krahmalyov <sergey.krahmaliov@gmail.com>
2026-02-20 01:16:19 +0400

    Проект развёрнут, добавлены базовые и защищённые роуты, добавлен инструмент для управления модальными окнами, добавлена инструкция по развёртке проекта в readme файл, настроен линтер и претиер, добавлены алиасы

---
+++ src/app/providers/lib/data.tsx
@@ -0,0 +1,5 @@
+
+export const DATA_MODALS = {
+  'function-not-working': <></>,
+  'sign-out': <></>,
+};
---
+++ src/app/store/store.ts
@@ -0,0 +1,28 @@
+import { configureStore, combineReducers, PreloadedState } from '@reduxjs/toolkit';
+import { useDispatch, useSelector, useStore, TypedUseSelectorHook } from 'react-redux';
+
+import { modalReducer } from '@app/providers/model/ModalProvider';
+export const rootReducer = combineReducers({
+      modalSelector: modalReducer,
+});
+
+export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
+  return configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware().concat([
+        
+      ]),
+    preloadedState,
+  });
+};
+
+export const store = setupStore();
+
+export type AppStore = ReturnType<typeof setupStore>;
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppDispatch = AppStore['dispatch'];
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useAppStore: () => AppStore = useStore;
